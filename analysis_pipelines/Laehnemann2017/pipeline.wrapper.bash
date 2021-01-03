66bfc0bcfe4f10eb7ab03cea7da5f2a81d0d60dd
David Laehnemann <david.laehnemann@hhu.de>
2021-01-03 17:28:32 +0100

    update Laehnemann2017 core pipeline setup

--- analysis_pipelines/Laehnemann2017/pipeline.wrapper.bash
+++ analysis_pipelines/Laehnemann2017/pipeline.wrapper.bash
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-# make conda work
-source /net/cancergenomics/miniconda3/etc/profile.d/conda.sh
-
 SNAKEMAKE_DIR="../"
+# directory where accessory scripts for pipeline rules live
+SCRIPTS_DIR="$SNAKEMAKE_DIR/scripts"
 # make scripts available
 PATH="$SCRIPTS_DIR:$PATH"
 # create a conda environment for your pipeline with all the necessary tools installed
-conda activate Benchmarking_ProSolo
+eval "$( conda shell.posix activate Benchmarking_ProSolo )"
 
 RUNS=( `ls *stderr.log` )
 STDERR=${#RUNS[@]}stderr.log
@@ -16,16 +15,17 @@ ENV=${#RUNS[@]}conda-package-list.txt
 
 conda list --export >$ENV
 
-#LOGS=sge_logs/${#RUNS[@]}/
-#
+#export GRB_LICENSE_FILE=/path/to/gurobi_license/gurobi.lic
+LOGS=sge_logs/${#RUNS[@]}/
+
 #mkdir -p $LOGS
 
 ##snakemake --cluster "qsub -V -cwd -pe multislot {threads} -q all.q -o ./$LOGS -e ./$LOGS" --use-conda --output-wait 61 --timestamp --stats $STATS --cores 180 2> >(tee $STDERR >&2)
 #
 ### alternatively use the below command if you have rules that specify a per-thread memory usage in GBs in resources.mem_gb
-#snakemake --cluster "qsub -V -cwd -pe smp {threads} -l h_vmem={resources.mem_gb}G -q compute.q -o ./$LOGS -e ./$LOGS" --output-wait 31 --stats $STATS --cores 108 2> >(tee $STDERR >&2)
-nice snakemake --use-conda --output-wait 31 --stats $STATS --cores 40 2> >(tee $STDERR >&2)
+#snakemake --cluster "qsub -V -cwd -pe smp {threads} -l h_vmem={resources.mem_gb}G -q compute.q -o ./$LOGS -e ./$LOGS" --use-conda --conda-frontend mamba --output-wait 68 --stats $STATS --cores 60 2> >(tee $STDERR >&2)
+nice snakemake --use-conda --conda-frontend mamba --resources mem_gb=300 --output-wait 31 --stats $STATS --cores 40 2> >(tee $STDERR >&2)
 
 ## alternatively use the below command for detailed debugging
 #snakemake --debug --timestamp 2> >(tee $STDERR >&2)
-conda deactivate
+eval "$( conda shell.posix deactivate )"
